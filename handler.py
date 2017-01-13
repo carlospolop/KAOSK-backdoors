56eb57d66f5b7c0536cf9ab61f8a4f616ba3b286
carlos <carlos@MacBook-Pro-de-Rhalyc.local>
2017-01-13 15:40:38 +0100

    terms

--- handler.py
+++ handler.py
@@ -1,4 +1,28 @@
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
+
+# Copyright (c) 2017, Carlos Polop Martin <carlospolop[at]gmail.com
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without modification, are permitted
+# provided that the following conditions are met:
+#
+# Redistributions of source code must retain the above copyright notice, this list of conditions and
+# the following disclaimer.
+#
+# Redistributions in binary form must reproduce the above copyright notice, this list of conditions
+# and the following disclaimer in the documentation and/or other materials provided with the
+# distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+# FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+# IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+# OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 import os
 import subprocess
 import time
@@ -272,6 +296,13 @@ def create_rc(payload, lport):
     print "Files RC and Ruby created!"
     return path_rc
 
+
+def create_file(path):
+    if not os.path.exists(path):
+        f = open(path,'w')
+        f.close()
+
+
 def start_service(service):
     try:
         cmd = "service "+service+" start"
@@ -280,10 +311,19 @@ def start_service(service):
     except subprocess.CalledProcessError as e:
         print "FAIL starting service: "+service
 
+
 ###############
 ## MAIN PART ##
 ###############
+print ">=>   >=>         >>           >===>        >=>>=>   >=>   >=>"
+print ">=>  >=>         >>=>        >=>    >=>   >=>    >=> >=>  >=>"
+print ">=> >=>         >> >=>     >=>        >=>  >=>       >=> >=>     "
+print ">>=>>          >=>  >=>    >=>        >=>    >=>     >>=>>       "
+print ">=>  >=>      >=====>>=>   >=>        >=>       >=>  >=>  >=>    "
+print ">=>   >=>    >=>      >=>    >=>     >=>  >=>    >=> >=>   >=>   "
+print ">=>     >=> >=>        >=>     >===>        >=>>=>   >=>     >=> "
 print ""
+
 if len(sys.argv) > 1:
     if ip_validator(sys.argv[1]):
         LHOST = sys.argv[1]
@@ -296,14 +336,14 @@ else:
 
 start_service("apache2")
 start_service("postgresql")
+create_file(moves_path)
 
 print "Lets gonna run a metasploit console because then the next one will start faster"
 comando = "msfconsole"
 os.system("gnome-terminal -e 'bash -c \""+comando+"; exec bash\"' 2> /dev/null")
-
+print "Listening..."
 while True:
     handle_payloads = moves_read()
     for payload in handle_payloads:
         handler(payload.strip())
     time.sleep(0.5)
-
